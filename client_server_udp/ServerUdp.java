b22eda2ee9ec7bc65d67176547c525a2ad24d5fe
sarath chandran m <sarathchandram2001@gmail.com>
2024-03-02 21:28:41 +0530

    third

--- client_server_udp/ServerUdp.java
+++ client_server_udp/ServerUdp.java
@@ -1,20 +1,26 @@
-//udp server
 import java.io.*;
 import java.net.*;
+
 public class ServerUdp {
     public static void main(String[] args) {
-        DatagramSocket ds=null;
-        DatagramPacket dp=null;
-        try{
-            ds=new DatagramSocket(1234);
-            byte[] buffer=new byte[1000];
-            dp= new DatagramPacket(buffer, buffer.length);
+        DatagramSocket ds = null;
+        DatagramPacket dp = null;
+        DatagramPacket reply = null;
+
+        try {
+            ds = new DatagramSocket(1234);
+            byte[] buffer = new byte[1000];
+            dp = new DatagramPacket(buffer, buffer.length);
             ds.receive(dp);
-            System.out.println("From Client :"+(new String(dp.getData()).trim()));
-            System.out.println("CLinet Port :"+dp.getPort());
-            rely=new DatagramSocket()
+            System.out.println("From Client: " + (new String(dp.getData()).trim()));
+            System.out.println("Client Port: " + dp.getPort());
+
+            reply = new DatagramPacket("from server ok".getBytes(), "from server ok".length(), dp.getAddress(), dp.getPort());
+            ds.send(reply);
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+        } finally {
+            if (ds != null) ds.close();
         }
     }
-
-    
 }
